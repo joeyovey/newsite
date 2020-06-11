b896b4019cf8b52daf1f5aa470c8cb4fb73fa8ba
Joey Ovey <josephovey6@gmail.com>
2020-06-11 22:49:46 +0100

    files added

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
-echo "Hello World";
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PHP Primer - Index</title>
+    </head>
+    <body>
+        <h2>Follow each link to the page sample</h2>
+        <ul>
+            <li><a href="array.php">Simple Array and Printouts</a></li>
+            <li><a href="forloop.php">Simple for Loop</a></li>
+            <li><a href="dowhileloop.php">Simple D while</a></li>
+            <li><a href="whileloop.php">Simple while loop</a></li>
+            <li><a href="ifstatement.php">Simple If Statement</a></li>
+            <li><a href="switchstatement.php">Simple Switch Statement</a></li>
 
 
-?>
+        </ul>
+    </body>
+</html>
