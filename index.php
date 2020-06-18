c036687a72cab34a43d2cef4322344ef122545d5
Joey Ovey <josephovey6@gmail.com>
2020-06-18 01:19:51 +0100

    added a link

--- index.php
+++ index.php
@@ -6,5 +6,7 @@
 <!-- basic HTML -->
 <h1>Hello HTML - PHP Primer</h1>
 <button type="button" class="btn btn-outline-success">Login</button>
+<button type="button" class="btn btn-outline-danger">Logout</button>
+
     
 <?php require "includes/footer.php" ?>
